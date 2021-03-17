1d26b6f34c40c30db5b5077c863c0cde9f49c712
VladimirSovkov <vladimir.sovkov@yandex.ru>
2021-03-18 00:33:20 +0300

    Добавил скрипты

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,12 +1,17 @@
 set PathToWebApp=..\Valuator\
 set PathToRankCalculator=..\RankCalculator\
+set PathToEventsLogger=..\EventsLogger\
 set PathToNginx=..\nginx\
 
 start "localhost:5001" /d %PathToWebApp% dotnet run --no-build --urls "http://localhost:5001"
 start "localhost:5002" /d %PathToWebApp% dotnet run --no-build --urls "http://localhost:5002"
 
-start /d %PathToRankCalculator% dotnet run --no-build
-start /d %PathToRankCalculator% dotnet run --no-build
+start "RankCalculator1" /d %PathToRankCalculator% dotnet run --no-build
+start "RankCalculator2" /d %PathToRankCalculator% dotnet run --no-build
+
+
+start "EventsLogger1" /d %PathToEventsLogger% dotnet run --no-build
+start "EventsLogger2" /d %PathToEventsLogger% dotnet run --no-build
 
 start "nginx" /d %PathToNginx% nginx.exe
 
